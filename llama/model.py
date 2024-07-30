dc196078f33de225238544d41237d21a9be7672c
travisddavies <travisddavies@gmail.com>
2024-07-30 11:56:34 +0800

    added computing frequencies cis funciton

--- llama/model.py
+++ llama/model.py
@@ -59,6 +59,7 @@ class RMSNorm(torch.nn.Module):
             torch.Tensor: The normalized tensor.
 
         """
+        # return the norm along the -1 axis
         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
 
     def forward(self, x):
@@ -73,4 +74,40 @@ class RMSNorm(torch.nn.Module):
 
         """
         output = self._norm(x.float()).type_as(x)
+        # multiply the calculated norm with the learnable weights
         return output * self.weight
+
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    """
+    Precompute the frequency tensor for complex exponentials (cis) with given dimensions.
+
+    This function calculates a frequency tensor with complex exponentials using the given dimension 'dim'
+    and the end index 'end'. The 'theta' parameter scales the frequencies.
+    The returned tensor contains complex values in complex64 data type.
+
+    Args:
+        dim (int): Dimension of the frequency tensor.
+        end (int): End index for precomputing frequencies.
+        theta (float, optional): Scaling factor for frequency computation. Defaults to 10000.0.
+
+    Returns:
+        torch.Tensor: Precomputed frequency tensor with complex exponentials.
+    """
+    # We first want to get the indices of the frequency values
+    # to be computed. In this case, it will be even numbers, 0 -> dim-2
+    freqs = torch.arange(0, dim, 2)
+    # We then want to slice it to ensure that it has the correct number of
+    # elements for the given dimension
+    freqs = freqs[: (dim // 2)]
+    freqs = freqs.float() / dim
+    freqs = 1.0 / (theta ** (freqs))
+    # Our timesteps
+    t = torch.arange(end, device=freqs.device)
+    # We then do an outer dot product to get our actual frequencies for
+    # every time step. Should be shape(len(t), len(freqs))
+    freqs = torch.outer(t, freqs).float()
+    # We then get the complex numbers of the frequencies.
+    # Magnitude is 1 (torch.ones_like) and phase is freqs
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_cis
