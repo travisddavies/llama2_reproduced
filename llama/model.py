0ca49c19e8b89ccf818c1bc1309e659c8b5fbac9
travisddavies <travisddavies@gmail.com>
2024-07-30 11:02:30 +0800

    added model file

---
+++ llama/model.py
@@ -0,0 +1,34 @@
+import math
+from dataclass import dataclass
+from typing import Optional, Tuple
+
+import fairscale.nn.model_parallel.initialize as fs_init
+import torch
+import torch.nn.functional as F
+from fairscale.nn.model_parallel.layers import (
+    ColumnParallelLinear,
+    ParallelEmbedding,
+    RowParallelLinear
+)
+
+
+@dataclass
+class ModelArgs:
+    # the dims for the embeddings
+    dim: int = 4096
+    # how many stacked transformers
+    n_layers: int = 32
+    # the number of key and value heads
+    n_kv_heads: Optional[int] = None
+    vocab_size: int = -1
+    multiple_of: int = 256
+    ffn_dim_multiplier: Optional[float] = None
+    # the epsilon value for RMS to ensure it's not zero
+    norm_eps: float = 1e-5
+
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6)
